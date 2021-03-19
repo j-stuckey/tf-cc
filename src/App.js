704aff89951d234f9fb288221f84eb298ef40832
James Stuckey <jstuckey@rutgers.edu>
2021-03-19 15:14:22 -0400

    Working on Modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import { useEffect, useState, useCallback } from 'react';
+import _ from 'lodash';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const GIPHY_API_KEY = '5Muqe6HOngq40S9xI6ZQJ7jDfvZUoS5f';
+
+    const [giphs, setGiphs] = useState([]);
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchGiphs, setSearchGiphs] = useState([])
+    const [isOpen, setIsOpen] = useState(false)
+
+    useEffect(() => {
+        async function fetchData() {
+            const res = await fetch(
+                `https://api.giphy.com/v1/gifs/trending?api_key=${GIPHY_API_KEY}`
+            );
+
+            const data = await res.json();
+
+            setGiphs(data.data);
+        }
+
+        fetchData();
+    }, []);
+
+    const debounceCallback = useCallback(
+        _.debounce(async (e) => {
+            const res = await fetch(
+                `https://api.giphy.com/v1/gifs/search?api_key=5Muqe6HOngq40S9xI6ZQJ7jDfvZUoS5f&q=${e.target.value}`
+            );
+            const data = await res.json();
+
+            setSearchGiphs(data.data);
+        }, 200),
+        []
+    );
+
+    const handleChange = (e) => {
+        const searchTerm = e.target.value;
+        debounceCallback(e);
+    };
+
+    return (
+        <div className="App">
+            <form>
+                <label htmlFor="search" />
+
+                <input type="search" name="search" onChange={handleChange} />
+            </form>
+            <Modal isOpen={isOpen} />
+            {searchGiphs.length ? <ul className="gif-list">
+                {searchGiphs.map((gif) => {
+                    const { images, username, rating } = gif;
+                    return (
+                        <li className="gif-image" onClick={() => {
+                                setIsOpen(true)
+                            }}>
+                            <img src={gif.images.fixed_height.url} />
+                        </li>
+                    );
+                })}
+            </ul> : <ul className="gif-list">
+                {giphs.map((gif) => {
+                    const { images, username, rating } = gif;
+                    return (
+                        <li className="gif-image" onClick={() => {
+                                setIsOpen(true)
+                            }}>
+                            <img src={gif.images.fixed_height.url} />
+                        </li>
+                    );
+                })}
+            </ul>}
+        </div>
+    );
 }
+
+function Modal({ data, isOpen }) {
+    if (isOpen) {
+        return <div>
+            <p>modal</p>
+        </div>
+    }
+    return null;
+};
 
 export default App;
